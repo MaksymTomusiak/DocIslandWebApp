b02dc1817573f6599822f55a0868499f13c261c6
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2025-06-03 21:13:37 +0300

    remove: remove debug log

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -15,7 +15,6 @@ import SignInPage from '../features/auth/SignInPage';
 import SignUpPage from '../features/auth/SignUpPage';
 
 const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-console.log(clerkKey);
 if (!clerkKey) {
     throw new Error('Missing Clerk Publishable Key');
 }
